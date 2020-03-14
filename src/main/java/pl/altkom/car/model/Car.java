ae6c26723450eb4880ba31c66c465733cc29b2c5
Brian <br.skrzynka@gmail.com>
2020-03-14 21:21:17 +0100

    Add car class

---
+++ src/main/java/pl/altkom/car/model/Car.java
@@ -0,0 +1,156 @@
+package pl.altkom.car.model;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import pl.altkom.car.model.Enums.Color;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity(name = "car")
+public class Car {
+
+    //FIELDS######################################################
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotEmpty(message = "*{first.correctnull}")
+    @Size(min = 2, max = 10, message = "*{first.correctlength}")
+    @Pattern(regexp = "[^@ ?!\\,$%]+", message = "*{first.correctregex}")
+    @Column(name = "brand")
+    private String brand;
+
+    @NotEmpty(message = "*{first.correctnull}")
+    @Size(min = 2, max = 10, message = "*{first.correctlength}")
+    @Pattern(regexp = "[^@ ?!\\,$%]+", message = "*{first.correctregex}")
+    @Column(name = "model")
+    private String model;
+
+    @NotEmpty(message = "*{first.correctnull}")
+    @Size(min = 2, max = 10, message = "*{first.correctlength}")
+    @Pattern(regexp = "[^@ ?!\\,$%]+", message = "*{first.correctregex}")
+    @Column(name = "VIN")
+    private String VIN;
+
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column(name = "date")
+    private LocalDate date;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "color")
+    private Color color;
+
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name="car_id")
+    private List<Route> routeList = new ArrayList<>(0);
+
+    //CONSTRUCTORS##################################################
+    public Car() {
+    }
+
+    public Car(String brand, String model, String VIN, LocalDate date, Color color, long id) {
+        this.brand = brand;
+        this.model = model;
+        this.VIN = VIN;
+        this.date = date;
+        this.color = color;
+        this.id = id;
+    }
+
+    public Car(String brand, String model, String VIN, LocalDate date, Color color) {
+        this.brand = brand;
+        this.model = model;
+        this.VIN = VIN;
+        this.date = date;
+        this.color = color;
+    }
+
+    //GETTERS&SETTERS##################################################################
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getVIN() {
+        return VIN;
+    }
+
+    public void setVIN(String VIN) {
+        this.VIN = VIN;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    //METHODS####################################################
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return id == car.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", VIN='" + VIN + '\'' +
+                ", date='" + date + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
+    public void addRoute(final Route route){
+
+        this.routeList.add(route);
+    }
+}
